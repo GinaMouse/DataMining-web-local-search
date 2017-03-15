fce6af70e2a1077d1e788a2395ba6b7924829f38
Lyle Denman <denmanlp@gmail.com>
2017-03-15 18:29:45 -0500

    Indexing and Searching is working

--- js/main.js
+++ js/main.js
@@ -1,67 +1,78 @@
-/*
-    This code was necessary to:
-    1. Create the initial index from the json data
-    2. Write the index (stringified) to a .json file
- */
-
+// /*
+//     This code was necessary to:
+//     1. Create the initial index from the json data
+//     2. Write the index (stringified) to a .json file
+//  */
+// CREATE INDEX
+// var lyricsIndex;
+// var store = {};
+//
 // require(['../node_modules/lunr/lunr.js'], function(lunr) {
-//        createIndex();
-
-// Create the index with desired fields
-//        function createIndex() {
-//            lyricsIndex = lunr(function () {
-//                this.ref('Rank');
+//     createIndex();
 //
-//                this.field('Song', { boost: 10 });
-//                this.field('Artist', { boost: 10 });
-//                this.field('Year');
-//                this.field('Lyrics', { boost: 5 });
-//            });
-//        }
-
-//        $.getJSON('../json/lyrics.json', function (data) {
+//     //Create the index with desired fields
+//     function createIndex() {
+//         lyricsIndex = lunr(function () {
+//             this.field('Song');
+//             this.field('Artist');
+//             this.field('Rank');
+//             this.field('Year');
+//             this.ref('Lyrics');
+//         });
+//     }
 //
-//            for (var i = 1; i < data.length; i++) {
+//     $.getJSON('../json/lyrics.json', function (data) {
 //
-//                var doc = {
-//                'Rank': data[i].FIELD1,
-//                'Song': data[i].FIELD2,
-//                'Artist': data[i].FIELD3,
-//                'Year': data[i].FIELD4,
-//                'Lyrics': data[i].FIELD5
-//                };
+//         for (var i = 1; i < data.length; i++) {
 //
-//                store[doc.Rank] = {
-//                    'Rank': doc.Rank
-//                };
+//             var doc = {
+//                 'Rank': data[i].FIELD1,
+//                 'Song': data[i].FIELD2,
+//                 'Artist': data[i].FIELD3,
+//                 'Year': data[i].FIELD4,
+//                 'Lyrics': data[i].FIELD5
+//             };
 //
-//                lyricsIndex.add(doc, true);
-//            }
-//            console.log(lyricsIndex);
-//            console.log(typeof lyricsIndex);
+//             store[doc.Lyrics] = {
+//                 'Rank': doc.Rank,
+//                 'Song': doc.Song,
+//                 'Artist': doc.Artist,
+//                 'Year': doc.Year
+//             };
 //
-//            var arr = JSON.stringify(lyricsIndex);
-//            console.log(typeof arr);
+//             lyricsIndex.add(doc, true);
+//         }
+//         console.log(lyricsIndex);
 //
-//            var blob = new Blob([arr], {type: "json"});
-//            saveAs(blob, "lyricsIndex.json");
+//         var arr = JSON.stringify({
+//             index: lyricsIndex,
+//             store: store
+//         });
 //
-//            var results = lyricsIndex.search("love is a battlefield");
+//         var blob = new Blob([arr], {type: "json"});
+//         saveAs(blob, "lyricsIndex.json");
 //
-//            console.log(results);
+//         var results = lyricsIndex.search("battlefield");
 //
-//            if (results.length == 0) {
-//                console.log("No results found.");
-//            } else {
-//                for (var j = 0; j < results.length; j++) {
-//                    document.getElementById("output").innerHTML += "<br>" +
-//                        "<b> Rank: " + results[j].ref +
-//                        store[results[j].ref].Song +
-//                    "</b><br>" +
-//                        "<br><br>";
+//         console.log(results);
+//
+//         if (results.length == 0) {
+//             console.log("No results found.");
+//         } else {
+//             for (var j = 0; j < results.length; j++) {
+//                 document.getElementById("output").innerHTML += "<br>" +
+//                     "<b> Song Title: " + store[results[j].ref].Song +
+//                     " Rank: " +
+//                     store[results[j].ref].Rank + " Year: " +
+//                     store[results[j].ref].Year + " Artist: " +
+//                     store[results[j].ref].Artist +
+//                     "</b><br>" +
+//                     "<br><br>";
 //
 //
-//                }
-//            }
+//             }
+//         }
 //
-//        });
+//     });
+//
+// });
