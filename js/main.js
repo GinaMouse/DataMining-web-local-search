67bc0de78b71f9c39a163cea77de4cd7528b6248
Lyle Denman <denmanlp@gmail.com>
2017-03-15 19:02:28 -0500

    Local Searching working properly

--- js/main.js
+++ js/main.js
@@ -1,130 +1,53 @@
-// /*
-//     This code was necessary to:
-//     1. Create the initial index from the json data
-//     2. Write the index (stringified) to a .json file
-//  */
-// CREATE INDEX
-// var lyricsIndex;
-// var store = {};
-//
-// require(['../node_modules/lunr/lunr.js'], function(lunr) {
-//     createIndex();
-//
-//     //Create the index with desired fields
-//     function createIndex() {
-//         lyricsIndex = lunr(function () {
-//             this.field('Song');
-//             this.field('Artist');
-//             this.field('Rank');
-//             this.field('Year');
-//             this.ref('Lyrics');
-//         });
-//     }
-//
-//     $.getJSON('../json/lyrics.json', function (data) {
-//
-//         for (var i = 1; i < data.length; i++) {
-//
-//             var doc = {
-//                 'Rank': data[i].FIELD1,
-//                 'Song': data[i].FIELD2,
-//                 'Artist': data[i].FIELD3,
-//                 'Year': data[i].FIELD4,
-//                 'Lyrics': data[i].FIELD5
-//             };
-//
-//             store[doc.Lyrics] = {
-//                 'Rank': doc.Rank,
-//                 'Song': doc.Song,
-//                 'Artist': doc.Artist,
-//                 'Year': doc.Year
-//             };
-//
-//             lyricsIndex.add(doc, true);
-//         }
-//         console.log(lyricsIndex);
-//
-//         var arr = JSON.stringify({
-//             index: lyricsIndex,
-//             store: store
-//         });
-//
-//         var blob = new Blob([arr], {type: "json"});
-//         saveAs(blob, "lyricsIndex.json");
-//
-//         var results = lyricsIndex.search("battlefield");
-//
-//         console.log(results);
-//
-//         if (results.length == 0) {
-//             console.log("No results found.");
-//         } else {
-//             for (var j = 0; j < results.length; j++) {
-//                 document.getElementById("output").innerHTML += "<br>" +
-//                     "<b> Song Title: " + store[results[j].ref].Song +
-//                     " Rank: " +
-//                     store[results[j].ref].Rank + " Year: " +
-//                     store[results[j].ref].Year + " Artist: " +
-//                     store[results[j].ref].Artist +
-//                     "</b><br>" +
-//                     "<br><br>";
-//
-//
-//             }
-//         }
-//
-//     });
-//
-// });
-/*
-    This code was necessary to:
-    1. Create the initial index from the json data
-    2. Write the index (stringified) to a .json file
- */
 
-// require(['../node_modules/lunr/lunr.js'], function(lunr) {
+//    var lyricsIndex;
+//    var storage = {};
+//
+//    require(["../node_modules/lunr/lunr.js"], function(lunr) {
 //        createIndex();
-
-// Create the index with desired fields
+//
+//        //Create the index with desired fields
 //        function createIndex() {
 //            lyricsIndex = lunr(function () {
-//                this.ref('Rank');
-//
-//                this.field('Song', { boost: 10 });
-//                this.field('Artist', { boost: 10 });
+//                this.field('Song');
+//                this.field('Artist');
+//                this.field('Rank', { boost: 10 });
 //                this.field('Year');
-//                this.field('Lyrics', { boost: 5 });
+//                this.ref('Lyrics');
 //            });
 //        }
-
+//
 //        $.getJSON('../json/lyrics.json', function (data) {
 //
 //            for (var i = 1; i < data.length; i++) {
 //
 //                var doc = {
-//                'Rank': data[i].FIELD1,
-//                'Song': data[i].FIELD2,
-//                'Artist': data[i].FIELD3,
-//                'Year': data[i].FIELD4,
-//                'Lyrics': data[i].FIELD5
+//                    'Rank': data[i].FIELD1,
+//                    'Song': data[i].FIELD2,
+//                    'Artist': data[i].FIELD3,
+//                    'Year': data[i].FIELD4,
+//                    'Lyrics': data[i].FIELD5
 //                };
 //
-//                store[doc.Rank] = {
-//                    'Rank': doc.Rank
+//                storage[doc.Lyrics] = {
+//                    'Rank': doc.Rank,
+//                    'Song': doc.Song,
+//                    'Artist': doc.Artist,
+//                    'Year': doc.Year
 //                };
 //
 //                lyricsIndex.add(doc, true);
 //            }
 //            console.log(lyricsIndex);
-//            console.log(typeof lyricsIndex);
 //
-//            var arr = JSON.stringify(lyricsIndex);
-//            console.log(typeof arr);
+//            var arr = JSON.stringify({
+//                index: lyricsIndex,
+//                store: storage
+//            });
 //
 //            var blob = new Blob([arr], {type: "json"});
-//            saveAs(blob, "lyricsIndex.json");
+////            saveAs(blob, "lyricsIndex.json");
 //
-//            var results = lyricsIndex.search("love is a battlefield");
+//            var results = lyricsIndex.search("battlefield");
 //
 //            console.log(results);
 //
@@ -133,9 +56,12 @@
 //            } else {
 //                for (var j = 0; j < results.length; j++) {
 //                    document.getElementById("output").innerHTML += "<br>" +
-//                        "<b> Rank: " + results[j].ref +
-//                        store[results[j].ref].Song +
-//                    "</b><br>" +
+//                        "<b> Song Title: " + storage[results[j].ref].Song +
+//                        " Rank: " +
+//                        [results[j].ref].Rank + " Year: " +
+//                        storage[results[j].ref].Year + " Artist: " +
+//                        storage[results[j].ref].Artist +
+//                        "</b><br>" +
 //                        "<br><br>";
 //
 //
@@ -143,3 +69,5 @@
 //            }
 //
 //        });
+//
+//    });
